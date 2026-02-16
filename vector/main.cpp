170d5f6fc486b90e8310556e09a7ec5a4ef8040c
Mantas <57765655+mantaszm@users.noreply.github.com>
2026-02-16 19:05:27 +0200

    Add demo mode and random grade generation

--- vector/main.cpp
+++ vector/main.cpp
@@ -4,9 +4,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <random>
 
 struct Studentas{
-    uint16_t namuDarbaiVid100{}; // vidurkis * 100
+    uint16_t namuDarbaiVid100{};
     uint16_t namuDarbaiMed100{};
     uint8_t egzaminas{};
     char vardas[14]{};
@@ -16,9 +17,21 @@ struct Studentas{
 int main() {
     std::vector<Studentas> studentai;
     Studentas temp;
+    bool demo = false;
+
+    std::cout <<"Ar cia yra demo? (y/n) default: n ";
+    char isDemo[2];
+    std::cin.getline(isDemo, 2);
+    if (isDemo[0] == 'y' || isDemo[0] == 'Y') {
+        demo = true;
+    }
 
     while (true) {
         std::string stringTemp;
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> distrib(0, 10);
 
         std::cout << "Norint baigti parasykite 'baigti'\nVardas: ";
         std::cin.getline(temp.vardas, 14);
@@ -31,68 +44,101 @@ int main() {
         int kiek = 0;
         std::vector<int> nd_pazymiai;
 
-        std::cout << "Iveskite namu darbu pazymius (atskirtus tarpais):";
-        std::getline(std::cin, stringTemp);
-        
-        size_t pos = 0;
+        if (!demo) {
+            std::cout << "Iveskite namu darbu pazymius (atskirtus tarpais): ";
+            std::getline(std::cin, stringTemp);
 
-        while (pos < stringTemp.size()) {
+            size_t pos = 0;
 
-        while (pos < stringTemp.size() && stringTemp[pos] == ' ')
-            pos++;
+            while (pos < stringTemp.size()) {
 
-        if (pos >= stringTemp.size()) break;
+                while (pos < stringTemp.size() && stringTemp[pos] == ' ')
+                    pos++;
 
-        size_t next = stringTemp.find(' ', pos);
-        std::string token;
+                if (pos >= stringTemp.size()) break;
 
-        if (next == std::string::npos) {
-            token = stringTemp.substr(pos);
-            pos = stringTemp.size();
-        } else {
-            token = stringTemp.substr(pos, next - pos);
-            pos = next + 1;
+                size_t next = stringTemp.find(' ', pos);
+                std::string token;
+
+                if (next == std::string::npos) {
+                    token = stringTemp.substr(pos);
+                    pos = stringTemp.size();
+                } else {
+                    token = stringTemp.substr(pos, next - pos);
+                    pos = next + 1;
+                }
+
+                try {
+                    int paz = std::stoi(token);
+
+                    if (paz < 0 || paz > 10) {
+                        std::cout << "Netinkamas pazymys (0-10). Ignoruojamas.\n";
+                        continue;
+                    }
+
+                    suma += paz;
+                    nd_pazymiai.push_back(paz);
+                    kiek++;
+                }
+                catch (...) {
+                    std::cout << "Netinkama ivestis: " << token << " (ignoruojama)\n";
+                }
+            }
         }
+        else {
+            std::cout <<"Namu darbu pazymiu skaicius: ";
+            std::string kiekNd;
+            std::getline(std::cin, kiekNd);
 
-        try {
-            int paz = std::stoi(token);
+            try {
+                int kiekND = std::stoi(kiekNd);
 
-            if (paz < 0 || paz > 10) {
-                std::cout << "Netinkamas pazymys (0-10). Ignoruojamas.\n";
+                for (int i = 0; i < kiekND; i++) {
+                    int paz = distrib(gen);
+                    suma += paz;
+                    nd_pazymiai.push_back(paz);
+                    kiek++;
+                }
+
+                temp.egzaminas = static_cast<uint8_t>(distrib(gen));
+            }
+            catch (...) {
+                std::cout <<"Ne skaicius!\n";
                 continue;
             }
-
-            suma += paz;
-            nd_pazymiai.push_back(paz);
-            kiek++;
         }
-        catch (...) {
-            std::cout << "Netinkama ivestis: " << token << " (ignoruojama)\n";
-        }
-    }
+        
         std::sort(nd_pazymiai.begin(), nd_pazymiai.end());
 
         double nd_med = 0.0;
 
-        if (kiek % 2 == 0 && kiek != 0)
+        if (kiek == 0) {
+            nd_med = 0.0;
+        }
+        else if (kiek % 2 == 0) {
             nd_med = (nd_pazymiai[kiek/2 - 1] + nd_pazymiai[kiek/2]) / 2.0;
-        else
+        }
+        else {
             nd_med = nd_pazymiai[kiek/2];
-        temp.namuDarbaiMed100 = static_cast<uint16_t>(nd_med * 100);
+        }
 
+        temp.namuDarbaiMed100 = static_cast<uint16_t>(nd_med * 100);
+        
         if (kiek > 0)
             temp.namuDarbaiVid100 =
                 static_cast<uint16_t>((static_cast<double>(suma) / kiek) * 100);
         else
             temp.namuDarbaiVid100 = 0;
-
-        std::cout << "Egzamino pazymys: ";
-        std::getline(std::cin, stringTemp);
-        temp.egzaminas = static_cast<uint8_t>(std::stoi(stringTemp));
+        
+        if (!demo) {
+            std::cout << "Egzamino pazymys: ";
+            std::getline(std::cin, stringTemp);
+            temp.egzaminas = static_cast<uint8_t>(std::stoi(stringTemp));
+        }
 
         studentai.emplace_back(temp);
     }
-
+    
     std::cout << std::left
               << std::setw(14) << "Vardas"
               << std::setw(17) << "Pavarde"
