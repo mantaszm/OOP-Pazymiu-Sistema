30442607f9d6f6639f71d8f673922a9c7b7e1e3c
Mantas <57765655+mantaszm@users.noreply.github.com>
2026-03-04 01:51:40 +0200

    Add Studentas struct and function declarations

--- masyvas/funkcijos.h
+++ masyvas/funkcijos.h
@@ -1,1 +1,24 @@
+#ifndef OOP_PAZYMIU_SISTEMA_FUNKCIJOS_H
+#define OOP_PAZYMIU_SISTEMA_FUNKCIJOS_H
 
+#include <cstdint>
+#include <string>
+#include <algorithm>
+#include <cstring>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <random>
+
+struct Studentas{
+    uint16_t namuDarbaiVid100{};
+    uint16_t namuDarbaiMed100{};
+    uint8_t egzaminas{};
+    char vardas[14]{};
+    char pavarde[17]{};
+};
+
+Studentas* readTerminal(size_t& studentuKiekis);
+Studentas* readFile(const std::string& filename, size_t& studentuKiekis);
+
+#endif
