7bddc047c8f24e97a082d8c99ed0756ff0032042
Mantas <57765655+mantaszm@users.noreply.github.com>
2026-03-04 02:00:37 +0200

    Define Studentas struct and read functions

--- vector/funkcijos.h
+++ vector/funkcijos.h
@@ -1,1 +1,27 @@
+#ifndef OOP_PAZYMIU_SISTEMA_FUNKCIJOS_H
+#define OOP_PAZYMIU_SISTEMA_FUNKCIJOS_H
 
+#include <algorithm>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <chrono>
+#include <string>
+#include <random>
+#include <cstring>
+
+
+struct Studentas{
+    uint16_t namuDarbaiVid100{};
+    uint16_t namuDarbaiMed100{};
+    uint8_t egzaminas{};
+    char vardas[14]{};
+    char pavarde[17]{};
+};
+
+std::vector<Studentas> readFile(const std::string& filename);
+std::vector<Studentas> readTerminal();
+
+#endif
